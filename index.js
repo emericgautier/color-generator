6377800109158d5456305a8aae8831e606c66504
emericgautier <emericgtr@gmail.com>
2022-08-05 10:23:31 +0200

    setInterval function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// Créer 3 variables pour stocker 3 chiffres aléatoires
+setInterval(() => {
+  // Créer 3 variables pour stocker 3 chiffres aléatoires
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
 
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
+  const color = `rgb(${r}, ${g}, ${b})`;
 
-const color = `rgb(${r}, ${g}, ${b})`;
+  console.log(color);
 
-console.log(color);
+  // Donner au body une couleur de fond en rgb()
+  document.body.style.background = color;
 
-// Donner au body une couleur de fond en rgb()
-document.body.style.background = color;
-
-// Montrer sur le body la couleur rgb()
-document.body.innerHTML = `<h2>${color}</h2>`;
+  // Montrer sur le body la couleur rgb()
+  document.body.innerHTML = `<h2>${color}</h2>`;
+}, 1000);
