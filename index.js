ed77d5e3fd6ff19b9bee356614179b32c98ca9a0
emericgautier <emericgtr@gmail.com>
2022-08-05 09:14:04 +0200

    Create 3 variables to store 3 random numbers

---
+++ index.js
@@ -0,0 +1,13 @@
+// Créer 3 variables pour stocker 3 chiffres aléatoires
+
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
+
+const color = `rgb(${r}, ${g}, ${b})`;
+
+console.log(color);
+
+// Donner au body une couleur de fond en rgb()
+
+// Montrer sur le body la couleur rgb()
